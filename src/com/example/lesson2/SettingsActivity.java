c75e3b6f932033bf3b5ab1e221816642bf249ced
ying <ying@HP>
2014-02-23 22:33:48 -0600

    Added the broadcast receiver to the second screen.

--- src/com/example/lesson2/SettingsActivity.java
+++ src/com/example/lesson2/SettingsActivity.java
@@ -1,15 +1,25 @@
 package com.example.lesson2;
 
 import android.app.Activity;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements CounterBroadcastReceiver.CounterBroadcastReceiverListener{
+	private CounterBroadcastReceiver receiver;
+	private IntentFilter filter;
+	private TextView counterLabel;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
 		Log.d("SettingsActivity", "onCreate");
+		counterLabel = (TextView)this.findViewById(R.id.textView1);
+		receiver = new CounterBroadcastReceiver();
+		filter = new IntentFilter();
+		filter.addAction(CounterService.COUNTER_FILTER);
 	}
 	
 	@Override
@@ -23,12 +33,14 @@ public class SettingsActivity extends Activity {
 	protected void onResume() {
 		super.onResume();
 		Log.d("SettingsActivity", "onResume");
+		this.registerReceiver(receiver, filter);
 	}
 	
 	@Override
 	protected void onPause() {
 		super.onPause();
 		Log.d("SettingsActivity", "onPause"); 
+		this.unregisterReceiver(receiver);
 	}	
 
 	@Override
@@ -41,5 +53,11 @@ public class SettingsActivity extends Activity {
 	protected void onDestroy() {
 		super.onStart();
 		Log.d("SettingsActivity", "onDestroy");
+	}
+
+	@Override
+	public void counterReceived(int counter) {
+		counterLabel.setText(String.valueOf(counter));
+		
 	}	
 }
